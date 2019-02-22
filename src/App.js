8df63bf3708a85586594fd22a5470b691ed23f01
Pratik <pratik.kulshreshth@pepcus.com>
2019-02-22 21:47:26 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -3,23 +3,56 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isResizing: false,
+    }
+  }
+
+  onDrag(e) {
+    console.log('e --- ', e);
+  }
+
+  onDragMove(e) {
+    console.log('mouse move -- e -- ', e);
+  }
+
+  renderGhostBar() {
+    if (this.state.isResizing) {
+      return (
+        <div className="resize-ghost-line" />
+      );
+    }
+
+    return null;
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="App grid">
+        <div className="grid-header">
+          <div className="cell cell-first">
+            <span>First</span>
+            <div className="resize-column-line" onMouseDown={this.onDrag} onMouseMove={this.onDragMove} />
+            {this.renderGhostBar()}
+          </div>
+          <div className="cell cell-second">
+            <span>Second</span>
+            <div className="resize-column-line" />
+          </div>
+        </div>
+        <div className="grid-body">
+          <div className="grid-row">
+            <div className="cell cell-first">R1-First</div>
+            <div className="cell cell-second">R1-Second</div>
+          </div>
+          <div className="grid-row">
+            <div className="cell cell-first">R2-First</div>
+            <div className="cell cell-second">R2-Second</div>
+          </div>
+        </div>
       </div>
     );
   }
